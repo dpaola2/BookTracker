54c43594a811ba77f4df07155d0b3df191d28265
Dave Paola <dpaola2@gmail.com>
2023-06-12 10:20:44 -0400

    books and shelves belong to users

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,9 +9,11 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  shelf_id   :integer
+#  user_id    :integer
 #
 class Book < ApplicationRecord
   belongs_to :shelf
+  belongs_to :user
   has_many :isbn_search_results, dependent: :destroy
 
   has_one_attached :image
--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -6,7 +6,9 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 class Shelf < ApplicationRecord
   has_many :books
+  belongs_to :user
 end
