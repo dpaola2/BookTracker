a1b56a010cf94575c5f68d5162893369653d769f
Dave Paola <dpaola2@gmail.com>
2023-06-12 10:17:21 -0400

    new default route, fix shelf_id selected, closes #9

--- app/helpers/shelves_helper.rb
+++ app/helpers/shelves_helper.rb
@@ -1,2 +1,5 @@
 module ShelvesHelper
+  def shelves_for_select(user)
+    Shelf.all.map { |shelf| [shelf.name, shelf.id] }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   
   resources :shelves
   
-  root "books#index"
+  root "shelves#index"
 end
